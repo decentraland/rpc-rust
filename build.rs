8f77700f1b2396b951c74359ed3d2d8924e18580
guidota <guido@tamborindeguy.com.ar>
2022-10-25 16:02:47 -0300

    fix: clippy warning

--- build.rs
+++ build.rs
@@ -3,8 +3,9 @@ extern crate protoc_rust;
 fn main() {
     protoc_rust::Codegen::new()
         .out_dir("src/protocol")
-        .inputs(&["src/protocol/index.proto"])
+        .inputs(["src/protocol/index.proto"])
         .include("src/protocol")
         .run()
         .expect("Running protoc failed.");
 }
+
