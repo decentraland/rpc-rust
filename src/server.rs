9d6458aedf3e4e9765bab189e411398451f2608c
guidota <guido@tamborindeguy.com.ar>
2022-11-15 17:22:32 -0300

    feat(server): add function handler and create port handle impl (wip)

--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,24 @@
-use protobuf::Message;
+use std::collections::HashMap;
+
+use protobuf::{Message, ProtobufError};
 
 use crate::{
     protocol::{
-        index::{CreatePort, DestroyPort, Request, RequestModule, RpcMessageTypes},
+        index::{
+            CreatePort, CreatePortResponse, DestroyPort, Request, RequestModule, RpcMessageTypes,
+        },
         parse::parse_header,
     },
     transports::{Transport, TransportEvent},
 };
 
+type PortHandlerFn = dyn Fn(&mut RpcServerPort) + Send + Sync + 'static;
+
 pub struct RpcServer {
     transport: Option<Box<dyn Transport>>,
-    handler: Option<Box<dyn Fn(RpcServerPort)>>,
+    handler: Option<Box<PortHandlerFn>>,
 }
-
-pub struct RpcServerPort {}
-
 impl RpcServer {
-    // TODO: allow multiple transports
     pub fn create() -> Self {
         Self {
             transport: None,
@@ -29,14 +31,23 @@ impl RpcServer {
     }
 
     pub async fn run(&mut self) {
+        let mut ports: HashMap<u32, RpcServerPort> = HashMap::new();
         loop {
-            match self.transport.as_mut().expect("No transport attached").receive().await {
+            match self
+                .transport
+                .as_mut()
+                .expect("No transport attached")
+                .receive()
+                .await
+            {
                 Ok(event) => {
                     if let TransportEvent::Connect = event {
                         println!("Transport connected");
                     } else if let TransportEvent::Message(payload) = event {
                         println!("Transport new data");
-                        self.handle_message(payload);
+                        self.handle_message(payload, &mut ports)
+                            .await
+                            .expect("can handle message");
                     } else if let TransportEvent::Error(err) = event {
                         println!("Transport error {}", err);
                     } else if let TransportEvent::Close = event {
@@ -52,32 +63,56 @@ impl RpcServer {
         }
     }
 
-    pub fn set_handler(&mut self, handler: Box<dyn Fn(RpcServerPort) + 'static + Send>) {
-        self.handler = Some(handler);
+    pub fn set_handler<H: Fn(&mut RpcServerPort) + Send + Sync + 'static>(&mut self, handler: H) {
+        self.handler = Some(Box::new(handler));
     }
 
-    fn handle_message(&self, payload: Vec<u8>) {
+    async fn handle_message(
+        &self,
+        payload: Vec<u8>,
+        ports: &mut HashMap<u32, RpcServerPort>,
+    ) -> Result<(), ProtobufError> {
         let transport = self.transport.as_ref().expect("Transport not attached");
         let header = parse_header(&payload);
 
         match header {
-            Some((message_type, message_number)) => {
+            Some((message_type, message_identifier)) => {
                 match message_type {
                     RpcMessageTypes::RpcMessageTypes_REQUEST => {
-                        let request = Request::parse_from_bytes(&payload);
-                        print!("Request");
+                        let request = Request::parse_from_bytes(&payload)?;
+                        if let Some(port) = ports.get(&request.port_id) {
+
+                        }
+                        print!("Request {:?}", request);
                     }
                     RpcMessageTypes::RpcMessageTypes_REQUEST_MODULE => {
-                        let request_module = RequestModule::parse_from_bytes(&payload);
-                        print!("RequestModule");
+                        let request_module = RequestModule::parse_from_bytes(&payload)?;
+                        print!("RequestModule {:?}", request_module);
                     }
                     RpcMessageTypes::RpcMessageTypes_CREATE_PORT => {
-                        let create_port = CreatePort::parse_from_bytes(&payload);
-                        print!("CreatePort");
+                        let port_id = (ports.len() + 1) as u32;
+                        let create_port = CreatePort::parse_from_bytes(&payload)?;
+                        print!("CreatePort {:?}", create_port);
+                        let port_name = create_port.port_name;
+                        let mut port = RpcServerPort::new(port_name.clone());
+                        
+                        if let Some(handler) = &self.handler {
+                            handler(&mut port);
+                        }
+
+                        ports.insert(port_id, port);
+
+                        let mut response = CreatePortResponse::new();
+                        response.message_identifier = message_identifier;
+                        response.port_id = port_id;
+                        transport
+                            .send(response.write_to_bytes()?)
+                            .await
+                            .expect("message to be sent");
                     }
                     RpcMessageTypes::RpcMessageTypes_DESTROY_PORT => {
-                        let destroy_port = DestroyPort::parse_from_bytes(&payload);
-                        print!("DestroyPort");
+                        let destroy_port = DestroyPort::parse_from_bytes(&payload)?;
+                        print!("DestroyPort {:?}", destroy_port);
                     }
                     RpcMessageTypes::RpcMessageTypes_STREAM_ACK
                     | RpcMessageTypes::RpcMessageTypes_STREAM_MESSAGE => {
@@ -91,11 +126,26 @@ impl RpcServer {
             None => {
                 println!("Invalid header");
             }
-        }
+        };
+        Ok(())
     }
+}
+type RequestHandler = dyn Fn(&[u8]);
+pub struct RpcServerPort {
+    pub name: String,
+    pub handlers: HashMap<String, Box<RequestHandler>>,
 }
 
 impl RpcServerPort {
+    pub fn new(name: String) -> Self {
+        RpcServerPort {
+            name,
+            handlers: HashMap::new(),
+        }
+    }
+    pub fn register<H: Fn(&[u8]) + 'static>(&mut self, name: String, handler: H) {
+        self.handlers.insert(name, Box::new(handler));
+    }
     fn load_module(&self) {
         todo!()
     }
--- tests/server.rs
+++ tests/server.rs
@@ -1,32 +1,29 @@
-use rpc_rust::protocol::index::CreatePort;
 use rpc_rust::server::{RpcServer, RpcServerPort};
 use rpc_rust::transports::memory::MemoryTransport;
-use rpc_rust::transports::Transport;
 
-struct BookContext
-{
-    books: Vec<String>
+struct BookContext {
+    books: Vec<String>,
 }
 
 #[tokio::test]
 async fn call_procedure() {
     // 1- Create Transport
 
-    let (client_transport, server_transport) = MemoryTransport::create();
-    
+    let (_client_transport, server_transport) = MemoryTransport::create();
+
     // 2- Create Server with Transport
     let mut server = RpcServer::create();
 
     // 3- Server listen to Create Port request
-    let handler = Box::new(|port: RpcServerPort|{
-        println!("Port created!");
-        /*port.register("GetBook", |request, context|{
-            return GetBookResponse();
-        });*/
+    server.set_handler(|port: &mut RpcServerPort| {
+        println!("Port {} created!", port.name);
+        port.register("GetBook".to_string(), |request|{
+
+            // return GetBookResponse();
+        });
     });
-    server.set_handler(handler);
-    
-    server.attach_transport(server_transport)
+
+    server.attach_transport(server_transport);
 
     server.run().await
 
@@ -38,12 +35,11 @@ async fn call_procedure() {
         // 5- Load Module
         let book_module = port.load_module("BookService").await;
         // 6- Call Procedure
-        let payload = BookRequest.toBytes();// GetBook as bytes 
+        let payload = BookRequest.toBytes();// GetBook as bytes
         let response = book_module.callUnary("GetBook", payload).await;
 
         let book_service = ClientBookServiceCodeGen.new(book_module);
         let response = book_service.GetBook(payload).await;
         // response should be GetBookResponse as bytes
     });*/
-
 }
