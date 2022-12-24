9da6705616980b7dee88aac1d29f09d4ab5a4924
lauti7 <lautigbustos@gmail.com>
2022-12-24 15:42:39 -0300

    feat(example): make service async

--- examples/integration/codegen/mod.rs
+++ examples/integration/codegen/mod.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use protobuf::Message;
 use rpc_rust::{server::RpcServerPort, types::ServiceModuleDefinition};
 
@@ -8,8 +10,9 @@ use crate::{
 
 pub const SERVICE: &str = "BookService";
 
+#[async_trait::async_trait]
 pub trait BookServiceInterface {
-    fn get_book(&self, request: GetBookRequest, context: &MyExampleContext) -> Book;
+    async fn get_book(&self, request: GetBookRequest, context: Arc<MyExampleContext>) -> Book;
 }
 
 pub struct BookServiceCodeGen {}
@@ -21,10 +24,18 @@ impl BookServiceCodeGen {
     ) {
         println!("> BookServiceCodeGen > register_service");
         let mut service_def = ServiceModuleDefinition::new();
+        // Share service ownership
+        let service = Arc::new(service);
         service_def.add_definition("GetBook".to_string(), move |request, context| {
-            let res = service.get_book(GetBookRequest::parse_from_bytes(request).unwrap(), context);
-            println!("> Service Definition > Get Book > response: {:?}", res);
-            res.write_to_bytes().unwrap()
+            let req = Vec::from(request);
+            let serv = service.clone();
+            Box::pin(async move {
+                let res = serv
+                    .get_book(GetBookRequest::parse_from_bytes(&req).unwrap(), context)
+                    .await;
+                println!("> Service Definition > Get Book > response: {:?}", res);
+                res.write_to_bytes().unwrap()
+            })
         });
         port.register_module(SERVICE.to_string(), service_def)
     }
--- examples/integration/service/book_service.rs
+++ examples/integration/service/book_service.rs
@@ -1,21 +1,30 @@
+use std::{sync::Arc, time::Duration};
+
+use tokio::time::sleep;
+
 use crate::{codegen::BookServiceInterface, MyExampleContext};
 
 use super::api::Book;
 
 pub struct BookService {}
 
+#[async_trait::async_trait]
 impl BookServiceInterface for BookService {
-    fn get_book(
+    async fn get_book(
         &self,
         request: super::api::GetBookRequest,
-        ctx: &MyExampleContext,
+        ctx: Arc<MyExampleContext>,
     ) -> super::api::Book {
         assert_eq!(ctx.hardcoded_database.len(), 2);
 
+        // Simulate DB operation
+        println!("> BookService > async get_book > simulating DB operation");
+        sleep(Duration::from_secs(1)).await;
         let book = ctx
             .hardcoded_database
             .iter()
             .find(|book_record| book_record.isbn == request.isbn);
+        println!("> BookService > async get_book > awaited DB operation");
 
         book.map(Book::clone).unwrap_or_default()
     }
