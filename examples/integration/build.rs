195401e7adad7ee3b1db1e28a93721ac2a8bcb1a
guidota <guido@decentraland.org>
2023-03-02 17:31:20 -0500

    feat: add client and server code generation for services

--- examples/integration/build.rs
+++ examples/integration/build.rs
@@ -1,10 +1,11 @@
-extern crate prost_build;
 use std::io::Result;
 
 fn main() -> Result<()> {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=src/service/index.proto");
 
-    prost_build::compile_protos(&["src/service/api.proto"], &["src/service"])?;
+    let mut conf = prost_build::Config::new();
+    conf.service_generator(Box::new(rpc_codegen::RPCServiceGenerator::new()));
+    conf.compile_protos(&["src/service/api.proto"], &["src/service"])?;
     Ok(())
 }
