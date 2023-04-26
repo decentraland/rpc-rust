66be8e3964f9ebf250c83abf1942472373a6c706
guidota <guido@tamborindeguy.com.ar>
2023-04-26 13:36:37 -0300

    chore: adding cargo features and protobuf-src (#69)
    Signed-off-by: guidota <guido@tamborindeguy.com.ar>

--- rpc/build.rs
+++ rpc/build.rs
@@ -2,6 +2,7 @@ extern crate prost_build;
 use std::io::Result;
 
 fn main() -> Result<()> {
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=src/rpc_protocol/index.proto");
 
--- rpc/src/transports/error.rs
+++ rpc/src/transports/error.rs
@@ -1,7 +1,9 @@
 //! Contains the parsing methods for the errors of the current transports.
 use std::{io, net::AddrParseError};
 
+#[cfg(feature = "quic")]
 use quinn::{ConnectError, ConnectionError, WriteError};
+#[cfg(feature = "websockets")]
 use tokio_tungstenite::tungstenite;
 
 use super::TransportError;
@@ -12,12 +14,14 @@ impl From<io::Error> for TransportError {
     }
 }
 
+#[cfg(feature = "quic")]
 impl From<ConnectError> for TransportError {
     fn from(_value: ConnectError) -> Self {
         TransportError::Internal
     }
 }
 
+#[cfg(feature = "quic")]
 impl From<ConnectionError> for TransportError {
     fn from(_value: ConnectionError) -> Self {
         TransportError::Internal
@@ -30,12 +34,14 @@ impl From<AddrParseError> for TransportError {
     }
 }
 
+#[cfg(feature = "quic")]
 impl From<WriteError> for TransportError {
     fn from(_value: WriteError) -> Self {
         TransportError::Connection
     }
 }
 
+#[cfg(feature = "websockets")]
 impl From<tungstenite::Error> for TransportError {
     fn from(_value: tungstenite::Error) -> Self {
         TransportError::Internal
