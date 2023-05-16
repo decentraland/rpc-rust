9f20e33b9a07c70784845c8d91d1dd7fb4144e6b
Lautaro Bustos <lautigbustos@gmail.com>
2023-05-16 12:12:41 -0300

    fix: failing codegen for server streams (#88)

--- examples/integration/build.rs
+++ examples/integration/build.rs
@@ -2,7 +2,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     // Tell Cargo that if the given file changes, to rerun this build script.
-    println!("cargo:rerun-if-changed=src/service/index.proto");
+    println!("cargo:rerun-if-changed=src/service/book.proto");
 
     let mut conf = prost_build::Config::new();
     conf.service_generator(Box::new(dcl_rpc::codegen::RPCServiceGenerator::new()));
